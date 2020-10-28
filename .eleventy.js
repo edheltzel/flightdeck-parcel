355c58a30e7a8835a1314710fae7c379b2e409ed
Ed Heltzel <ed@rainyday.media>
2020-10-28 15:44:47 -0400

    updating 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
-module.exports = {
-  dir: {
-    input: "views",
-    output: "dist"
-  }
-}
+module.exports = function (eleventyConfig) {
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+    },
+    templateFormats: ['html', 'md', 'njk', 'liquid'],
+    passthroughFileCopy: true,
+  };
+};
