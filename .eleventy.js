46438eee1baf7bebc238504f5932580d48053624
Ed Heltzel <ed@rainyday.media>
2022-02-09 13:55:18 -0500

    sass output dir change - adds flightdeck.manifest - sets sass output to `/asssets/css/` to leverage 11ty's `addPassthroughCopy`

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,18 @@
+const fd = require('./flightdeck.manifest');
 module.exports = function (eleventyConfig) {
   // watch for changes
-  eleventyConfig.addWatchTarget('./src/_scss');
+  eleventyConfig.addWatchTarget(fd.sass.src);
 
   // copy stuff
   eleventyConfig.addPassthroughCopy('./src/assets/css');
+  eleventyConfig.addPassthroughCopy('./src/assets/js');
+  eleventyConfig.addPassthroughCopy('./src/assets/images');
+  eleventyConfig.addPassthroughCopy('./src/assets/fonts');
+
+  // add collections
 
   // workflow
-  eleventyConfig.setBrowserSyncConfig({
-    open: true,
-  });
+  eleventyConfig.setBrowserSyncConfig(fd.bs);
   return {
     dir: {
       input: 'src',
---
+++ flightdeck.manifest.js
@@ -0,0 +1,11 @@
+module.exports = {
+  assets: './assets/',
+  sass: {
+    src: './src/_scss/**/*.scss',
+    dest: 'css',
+  },
+  bs: {
+    notify: true,
+    open: true,
+  },
+};
