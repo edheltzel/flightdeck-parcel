d62441956a93b886fa38c38bf4c459cc33d1414b
Ed Heltzel <ed@rainyday.media>
2022-02-14 13:15:49 -0500

    scaffold clean up

--- flightdeck.manifest.js
+++ flightdeck.manifest.js
@@ -1,9 +1,19 @@
+// const htmlmin = require('html-minifier');
+
 module.exports = {
   assets: './assets/',
   sass: {
     src: './src/_scss/**/*.scss',
-    dest: 'css',
+    dest: './_site/assets/css',
   },
+  js: {
+    src: './src/assets/js/',
+    dest: './_site/assets/css',
+  },
+  img{
+    src: './src/assets/images',
+    dest: './src/assets/images', //11ty addPassthroughCopy will move this
+  }
   bs: {
     notify: true,
     open: true,
