d1545688754844e5bb4e437337e428be64cfe319
Ed Heltzel <ed@rainyday.media>
2022-05-10 17:09:14 -0400

    removes minify html

--- .eleventy.js
+++ .eleventy.js
@@ -28,24 +28,6 @@ module.exports = function (config) {
 
   // add collections
 
-  // //minify html only for in production
-  // const isProd = process.env.ELEVENTY_ENV === 'prod';
-  // const minifyHtml = function (value, outputPath) {
-  //   if (outputPath && outputPath.indexOf('.html') > -1) {
-  //     return htmlmin.minify(value, {
-  //       collapseWhitespace: true,
-  //       collapseInlineTagWhitespace: true,
-  //       minifyCSS: true,
-  //       removeComments: true,
-  //       userShortDoctype: true,
-  //     });
-  //   }
-  // };
-
-  // if (isProd) {
-  //   config.addTransform('htmlmin', minifyHtml);
-  // }
-
   // launch browser on start
   config.setBrowserSyncConfig(fd.workflow.bs);
 
