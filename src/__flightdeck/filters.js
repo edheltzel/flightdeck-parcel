54c5cba51485d0ab32b54ea376a69a535a4df220
Ed Heltzel <ed@rainyday.media>
2022-05-23 14:51:21 -0400

    better filter names for dates

--- src/__flightdeck/filters.js
+++ src/__flightdeck/filters.js
@@ -2,15 +2,17 @@
  * TODO: see if these are worth adding 👉 https://www.aleksandrhovhannisyan.com/blog/useful-11ty-filters/
  *
  */
+
+// Human readable date & time - https://momentjs.com/timezone/docs/#/data-formats/
 const moment = require("moment-timezone");
 
 module.exports = (config) => {
-  config.addFilter("common", (date) => {
+  config.addFilter("basicDate", (date) => {
     const utc = date.toUTCString();
     return moment.utc(utc).format("MMM DD, YYYY");
   });
-  config.addFilter("dateAndTime", (date) => {
+  config.addFilter("fullDate", (date) => {
     const utc = Date.now();
-    return moment.utc(utc).tz("America/New_York").format("YYYY-MM-DD, HH:mm:ss z");
+    return moment.utc(utc).tz("America/New_York").format("YYYY-MM-DD, HH:mm:ss");
   });
 };
