2175b05b22c2fba466110a99c326cbd452ccb38e
Ed Heltzel <ed@rainyday.media>
2022-05-26 20:18:39 -0400

    updates workflow to include BS 404 callback

--- src/__flightdeck/workflows.js
+++ src/__flightdeck/workflows.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 module.exports = (config) => {
   config.setQuietMode(true); // reduce console
   /**
@@ -6,6 +8,18 @@ module.exports = (config) => {
   config.setBrowserSyncConfig({
     open: true,
     notify: true,
+    // 404
+    callbacks: {
+      ready: function (err, browserSync) {
+        const content_404 = fs.readFileSync("dist/404.html");
+
+        browserSync.addMiddleware("*", (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      },
+    },
   });
 
   // watch & copy stuff
