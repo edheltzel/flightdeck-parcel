e711f193a603200bbf5b61774a71ddaec6f343c2
Ed Heltzel <ed@rainyday.media>
2022-06-20 09:47:12 -0400

    update config to follow the same naming convention

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,13 @@
-const transforms = require("./src/__flightdeck/transforms");
-const workflow = require("./src/__flightdeck/workflow");
-
+const addTransforms = require("./src/__flightdeck/transforms");
+const addWorkflow = require("./src/__flightdeck/workflow");
 const addShortcodes = require("./src/__flightdeck/shortcodes");
 const addFilters = require("./src/__flightdeck/filters");
 
 module.exports = (config) => {
-  // workflow - browsersync, layout aliases, watch, passthrough copy
-  config.addPlugin(workflow);
-
-  // transforms - esbuild, sass, htmlmin
-  config.addPlugin(transforms);
-
-  // filters - universal filters
-  addFilters(config);
-
-  // shortcodes - copyright year, youtube embeds, etc.
-  addShortcodes(config);
+  addWorkflow(config); // workflow - browsersync, layout aliases, watch, passthrough copy
+  addTransforms(config); // transforms - esbuild, sass, htmlmin
+  addFilters(config); // filters - universal filters
+  addShortcodes(config); // shortcodes - copyright year, youtube embeds, etc.
 
   return {
     dir: {
