cccaeb3bee4bcd259500319b24840894fb27798e
Ed Heltzel <ed@rainyday.media>
2022-06-24 15:51:40 -0400

    esbuild for bundling js via parcel. commenting our esbuild transformer import update paths for dev and build in package.json workspace dictionary and packup installs

--- src/__flightdeck/transforms.js
+++ src/__flightdeck/transforms.js
@@ -1,52 +1,51 @@
-const esbuild = require("esbuild");
-const { sassPlugin } = require("esbuild-sass-plugin");
-const postcss = require("postcss");
-const autoprefixer = require("autoprefixer");
-const postcssPresetEnv = require("postcss-preset-env");
-const htmlmin = require("html-minifier");
+// const esbuild = require("esbuild");
+// const { sassPlugin } = require("esbuild-sass-plugin");
+// const postcss = require("postcss");
+// const autoprefixer = require("autoprefixer");
+// const postcssPresetEnv = require("postcss-preset-env");
+// const htmlmin = require("html-minifier");
 
 const isProd = process.env.ELEVENTY_ENV === "production";
 
 module.exports = (config) => {
   // esbuild & sass + autoprefixer
-  config.on("eleventy.after", () => {
-    return esbuild.build({
-      bundle: true,
-      entryPoints: {
-        "assets/js/app": "./src/assets/js/app.js",
-        "assets/css/app": "./src/assets/scss/app.scss",
-      },
-      loader: { ".scss": "css" },
-      minify: isProd,
-      outdir: "./dist",
-      plugins: [
-        sassPlugin({
-          async transform(source, resolveDir) {
-            const { css } = await postcss([
-              autoprefixer,
-              postcssPresetEnv({ stage: 0 }),
-            ]).process(source, { from: undefined });
-            return css;
-          },
-        }),
-      ],
-      sourcemap: !isProd,
-    });
-  });
-
+  // config.on("eleventy.after", () => {
+  //   return esbuild.build({
+  //     bundle: true,
+  //     entryPoints: {
+  //       "assets/js/app": "./src/assets/js/app.js",
+  //       "assets/css/app": "./src/assets/scss/app.scss",
+  //     },
+  //     loader: { ".scss": "css" },
+  //     minify: isProd,
+  //     outdir: "./dist",
+  //     plugins: [
+  //       sassPlugin({
+  //         async transform(source, resolveDir) {
+  //           const { css } = await postcss([
+  //             autoprefixer,
+  //             postcssPresetEnv({ stage: 0 }),
+  //           ]).process(source, { from: undefined });
+  //           return css;
+  //         },
+  //       }),
+  //     ],
+  //     sourcemap: !isProd,
+  //   });
+  // });
   // minify HTML
-  if (isProd) {
-    config.addTransform("htmlMin", async function (content, outputPath) {
-      if (outputPath && outputPath.endsWith(".html")) {
-        let minified = htmlmin.minify(content, {
-          collapseWhitespace: true,
-          minifyCSS: true,
-          removeComments: true,
-          useShortDoctype: true,
-        });
-        return minified;
-      }
-      return content;
-    });
-  }
+  // if (isProd) {
+  //   config.addTransform("htmlMin", async function (content, outputPath) {
+  //     if (outputPath && outputPath.endsWith(".html")) {
+  //       let minified = htmlmin.minify(content, {
+  //         collapseWhitespace: true,
+  //         minifyCSS: true,
+  //         removeComments: true,
+  //         useShortDoctype: true,
+  //       });
+  //       return minified;
+  //     }
+  //     return content;
+  //   });
+  // }
 };
