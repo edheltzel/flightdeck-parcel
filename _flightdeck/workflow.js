0ce63f70016cf36e1238df4cb4829f93cfc94c68
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-02-27 14:32:20 -0500

    updated flightdeck workflow

--- _flightdeck/workflow.js
+++ _flightdeck/workflow.js
@@ -3,29 +3,28 @@ const fs = require("fs");
 const isProd = process.env.ENV === "production";
 
 module.exports = (config) => {
-  config.setQuietMode(true); // reduce console output
-  config.setServerOptions({
-    /**
-     * @link https://github.com/11ty/eleventy/issues/1305
-     * @see https://www.11ty.dev/docs/dev-server/
-     * @description local dev server runs on port 8080 by default-> http://localhost:8080
-    */
-    showVersion: true, // show the server version number on the command line
-    showAllHosts: true, // show local network IP address for device testing
-    injectedScriptsFolder: ".flightdeck",
+	config.setQuietMode(true); // reduce console output
+	config.setServerOptions({
+		/**
+		 * @link https://github.com/11ty/eleventy/issues/1305
+		 * @see https://www.11ty.dev/docs/dev-server/
+		 * @description local dev server runs on port 8080 by default-> http://localhost:8080
+		 */
+		showVersion: true, // show the server version number on the command line
+		showAllHosts: false, // show local network IP address for device testing
+		injectedScriptsFolder: ".flightdeck",
+	});
 
-  });
+	// Watch Targets
+	config.addWatchTarget("./src/assets");
 
-  // Watch Targets
-  config.addWatchTarget("./src/assets");
+	// Passthrough Copy
+	config.addPassthroughCopy("./src/assets/fonts");
+	config.addPassthroughCopy({ "./src/_static": "./" }); // root level files ie: _redirects, robots.txt, etc
+	// config.addPassthroughCopy({ "./node_modules/alpinejs/dist/cdn.min.js": "assets/js/alpine.js" });
 
-  // Passthrough Copy
-  config.addPassthroughCopy("./src/assets/fonts");
-  config.addPassthroughCopy({ "./src/_static": "./" }); // root level files ie: _redirects, robots.txt, etc
-  // config.addPassthroughCopy({ "./node_modules/alpinejs/dist/cdn.min.js": "assets/js/alpine.js" });
-
-  // Layout Aliases
-  config.addLayoutAlias("default", "layouts/default.njk");
-  config.addLayoutAlias("post", "layouts/post.njk"); // consider using nunjucks extend
-  config.addLayoutAlias("page", "layouts/page.njk"); // consider using nunjucks extend
+	// Layout Aliases
+	config.addLayoutAlias("default", "layouts/default.njk");
+	config.addLayoutAlias("post", "layouts/post.njk"); // consider using nunjucks extend
+	config.addLayoutAlias("page", "layouts/page.njk"); // consider using nunjucks extend
 };
